491b96b83f68111667ab0cb57ac0de6ede5cd2a2
intensediscipline <intensediscipline@yahoo.com>
2023-05-11 08:08:50 +0100

    added logic for calculator app

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,43 @@
-alert('This works!');
+let currentResult = 0;
+
+function getNumberInput() {
+    return parseInt(userInput.value)
+}
+
+function writeAndCreateOutput(operator, resultBeforeCalc, calcNumber) {
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+    outputResult(currentResult, calcDescription);
+}
+
+function add() {
+    const enteredNumber = getNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult + enteredNumber;
+    writeAndCreateOutput('+', initialResult, enteredNumber);
+}
+
+function subtract() {
+    const enteredNumber = getNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult - enteredNumber;
+    writeAndCreateOutput('-', initialResult, enteredNumber);
+}
+
+function multiply() {
+    const enteredNumber = getNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult * enteredNumber;
+    writeAndCreateOutput('*', initialResult, enteredNumber);
+}
+
+function divide() {
+    const enteredNumber = getNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult / enteredNumber;
+    writeAndCreateOutput('/', initialResult, enteredNumber);
+}
+
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
